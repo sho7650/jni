26d30773328c81765229adcde66556a1b028c851
Takashi Abe <tabe@salesforce.com>
2017-06-09 17:26:27 +0900

    Fixed

--- src/main/java/JniSample.java
+++ src/main/java/JniSample.java
@@ -21,7 +21,7 @@ public class JniSample {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         JniSample s = new JniSample();
 	System.out.println("exp(1.0) -> " + s.exp(1.0));
 	System.out.println("exp(2.0) -> " + s.exp(2.0));
