b58b653cf21f0d2dc6656e24fb0c88dccae01d90
Takashi Abe <tabe@salesforce.com>
2017-06-09 13:24:56 +0900

    Add spring boot with maven

--- src/main/java/JniSample.java
+++ src/main/java/JniSample.java
@@ -1,6 +1,18 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
 public class JniSample {
     public native double exp(double a);
     public native boolean strcmp(String a, String b);
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         System.loadLibrary("JniSample");
@@ -9,5 +21,6 @@ public class JniSample {
 	System.out.println("exp(2.0) -> " + s.exp(2.0));
 	System.out.println("strcmp(\"Mutsuki\", \"Namajima\") -> " + s.strcmp("Mutsuki", "Nakajima"));
 	System.out.println("strcmp(\"Mutsuki\", \"Mutsuki\") -> " + s.strcmp("Mutsuki", "Mutsuki"));
+	SpringApplication.run(JniSample.class, args);
     }
 }
