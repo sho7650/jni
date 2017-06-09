e4494cd94d27ca2ecdf17e0050afcbb0777834db
Takashi Abe <tabe@salesforce.com>
2017-06-09 16:45:41 +0900

    Try

--- src/main/java/JniSample.java
+++ src/main/java/JniSample.java
@@ -1,28 +1,29 @@
 package com.jnisample;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.*;  
+import org.springframework.boot.autoconfigure.*;  
+import org.springframework.stereotype.*;  
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class JniSample {
     public native double exp(double a);
     public native boolean strcmp(String a, String b);
 
     @RequestMapping("/")
+    @ResponseBody
     String home() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) {
         System.loadLibrary("JniSample");
         JniSample s = new JniSample();
 	System.out.println("exp(1.0) -> " + s.exp(1.0));
 	System.out.println("exp(2.0) -> " + s.exp(2.0));
 	System.out.println("strcmp(\"Mutsuki\", \"Namajima\") -> " + s.strcmp("Mutsuki", "Nakajima"));
 	System.out.println("strcmp(\"Mutsuki\", \"Mutsuki\") -> " + s.strcmp("Mutsuki", "Mutsuki"));
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
 	SpringApplication.run(JniSample.class, args);
     }
 }
