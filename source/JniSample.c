88d2074eb03144f124d51e04622c1aad4ecfc56b
sho kisaragi <sho@oshiire.to>
2017-06-10 14:24:58 +0900

    Fix function name

--- source/JniSample.c
+++ source/JniSample.c
@@ -3,11 +3,11 @@
 #include <math.h>
 #include "JniSample.h"
 
-JNIEXPORT jdouble JNICALL Java_JniSample_exp (JNIEnv *env, jobject obj, jdouble jd1) {
+JNIEXPORT jdouble JNICALL Java_com_jnisample_JniSample_exp (JNIEnv *env, jobject obj, jdouble jd1) {
 	return exp(jd1);
 }
 
-JNIEXPORT jboolean JNICALL Java_JniSample_strcmp (JNIEnv *env, jobject obj, jstring js0, jstring js1) {
+JNIEXPORT jboolean JNICALL Java_com_jnisample_JniSample_strcmp (JNIEnv *env, jobject obj, jstring js0, jstring js1) {
 	const char* s0 = (*env)->GetStringUTFChars(env, js0, NULL);
 	const char* s1 = (*env)->GetStringUTFChars(env, js1, NULL);
 	return !strcmp(s0, s1);
