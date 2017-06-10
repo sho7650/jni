20142f8b20b27ead393e5a9120689692175ab92a
Takashi Abe <tabe@salesforce.com>
2017-06-10 17:44:07 +0900

    Delete command.sh

--- command.sh
+++
@@ -1,9 +0,0 @@
-vi JniSample.java
-javac JniSample.java
-/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home/bin/javah JniSample
-vi JniSample.h 
-vi JniSample.c
-cc -v -c -fPIC -I/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home/include/darwin JniSample.c -o libJniSample.o
-libtool -dynamic -lSystem libJniSample.o -o libJniSample.dylib
-export LD_LIBRARY_PATH=.
-java JniSample
